85be925fd705673115fa868a6172a1961d4247b6
yuya masumura <yuya@yuya-no-MacBook-Air.local>
2015-03-08 17:39:22 +0900

    first commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController1 : MonoBehaviour {
+
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonUp(KeyCode.A)) {
+			Debug.Log ("test");
+		}
+
+	}
+}
