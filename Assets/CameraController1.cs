c9e4c0f82fe8ee0875d1f006287569515109dac2
yuya masumura <yuya@yuya-no-MacBook-Air.local>
2015-03-11 18:18:08 +0900

    edit3_11

---
+++ Assets/CameraController1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController1 : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		
+	
+		if(Input.GetKeyUp(KeyCode.K)) {
+		//Debug.Log ("test");
+		transform.Rotate( 0, 90, 0);}
+
+		if(Input.GetKeyUp(KeyCode.L)) 	{
+		//Debug.Log ("test");
+		transform.Rotate(0, -90, 0);}
+
+	}
+}
