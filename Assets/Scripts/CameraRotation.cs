9fc873476c897aceb29eb672c9fc7607fd91b907
hal0taso <yuya7719guitar@gmail.com>
2017-05-16 09:16:00 +0900

    ソースの整理

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -2,31 +2,16 @@
 using System.Collections;
 
 public class CameraRotation : MonoBehaviour {
-	
-	public GameObject cube;
-	int Camera_Controller = 90;
-	
+
 	// Use this for initialization
 	void Start () {
-		
-		
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if (Input.GetKeyDown("a")) {
-			Camera_Controller += 90;
-			Camera_Controller %= 180;
-			Debug.Log(Camera_Controller);
-			iTween.RotateTo(cube, iTween.Hash("y", Camera_Controller, "time", 0.5f));
-		}
-		if (Input.GetKeyDown("d")) {
-			Camera_Controller -= 90;
-			Camera_Controller %= 180;
-			Debug.Log(Camera_Controller);
-			iTween.RotateTo(cube, iTween.Hash("y", Camera_Controller, "time", 0.5f));
-		}
-		
+			this.transform.Rotate (0, (Input.GetAxis ("Horizontal") * 1), 0);
+
 	}
+
 }
